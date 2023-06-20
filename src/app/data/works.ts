63772751c0d85dfc42a20a238570f5d44ad274b7
reshaditiya <reshaditiya@gmail.com>
2023-06-20 19:19:44 +0700

    works data setup

---
+++ src/app/data/works.ts
@@ -0,0 +1,30 @@
+type Works = {
+  name: string,
+  shortDesc: string,
+  longDesc: string
+}
+
+const works: Works[] = [
+  {
+    name: "My Health",
+    shortDesc: "Healthcare solution platform.",
+    longDesc: "Impressive Experience For A Wellness Platform.",
+  },
+  {
+    name: "Da Riil Estat´",
+    shortDesc: "Modern real estate platform.",
+    longDesc: "A brand new real estate listing website for on of the most renowned brokers in the south of Spain.",
+  },
+  {
+    name: "NFT Generator",
+    shortDesc: "Artificifial Intellegence website for NFT.",
+    longDesc: "Makes it easy for creators to create NFT assets.",
+  },
+  {
+    name: "Royal Garden",
+    shortDesc: "For the community of plant lovers.",
+    longDesc: "Grow a beautiful green garden and save the planet.",
+  },
+]
+
+export default works
