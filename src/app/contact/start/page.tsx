e74084a91cf6276e708099e41962322562e7e77e
reshaditiya <reshaditiya@gmail.com>
2023-06-21 21:08:00 +0700

    layout

---
+++ src/app/contact/start/page.tsx
@@ -0,0 +1,52 @@
+const projectTime = ["1 Month", "2 Months", "3 Months"]
+
+export default function page() {
+	return (
+		<>
+			<h1 className="text-h2">
+				When do you expect to start the project.
+			</h1>
+			<form className="mt-[2.5rem] flex flex-col gap-[1.5rem] md:gap-[2.25rem]">
+				<div className="flex flex-col gap-[1rem]">
+					<label className="text-body" htmlFor="budget">
+						Time to start project
+					</label>
+					<div
+						id="budget"
+						className="flex flex-wrap gap-[0.63rem] md:gap-[1rem]"
+					>
+						{projectTime.map((time) => (
+							<div key={time}>
+								<input
+									id={time}
+									type="radio"
+									className="btn-border peer hidden"
+									name="projectTime"
+								/>
+
+								<label
+									htmlFor={time}
+									className="btn-border peer-checked:btn-border-active block"
+								>
+									{time}
+								</label>
+							</div>
+						))}
+					</div>
+					<label
+						htmlFor="description"
+						className="text-body flex flex-col gap-[1rem]"
+					>
+						Anything else?
+						<textarea
+							id="description"
+							name="description"
+							className="input-text"
+							placeholder="Tell us about your project"
+						/>
+					</label>
+				</div>
+			</form>
+		</>
+	)
+}
