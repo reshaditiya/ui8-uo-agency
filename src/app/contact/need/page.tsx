eb1f1c000e6b155755589fed38cd764bfedc5a85
reshaditiya <reshaditiya@gmail.com>
2023-06-21 20:13:02 +0700

    remove redundance

--- src/app/contact/need/page.tsx
+++ src/app/contact/need/page.tsx
@@ -14,13 +14,25 @@ export default function page() {
 	return (
 		<>
 			<h1 className="text-h2">What do you need help with from us?</h1>
-			<div className="mt-[2.5rem] flex flex-wrap gap-[0.75rem] md:gap-[1.5rem]">
+			<form className="mt-[2.5rem] flex flex-wrap gap-[0.75rem] md:gap-[1.5rem]">
 				{services.map((service) => (
-					<button key={service} className="btn-border">
-						{service}
-					</button>
+					<div key={service}>
+						<input
+							id={service}
+							type="radio"
+							className="btn-border peer hidden"
+							name="need"
+						/>
+
+						<label
+							htmlFor={service}
+							className="btn-border peer-checked:btn-border-active block"
+						>
+							{service}
+						</label>
+					</div>
 				))}
-			</div>
+			</form>
 		</>
 	)
 }
