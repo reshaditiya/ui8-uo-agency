6b0aa9442a7e18a2a97274ea8675cd6fb9285a63
reshaditiya <reshaditiya@gmail.com>
2023-06-21 22:50:27 +0700

    contact context data setup

--- src/app/contact/layout.tsx
+++ src/app/contact/layout.tsx
@@ -3,10 +3,56 @@
 import Image from "next/image"
 import { usePathname } from "next/navigation"
 import { ContactAction } from "@/src/components"
-import { createContext } from "react"
+import { useReducer } from "react"
+import { ContactDataContext } from "@/src/components/Contexts"
 
 type ImageLayout = {
 	[key: string]: string
+}
+
+const contactDataInit = {
+	need: "",
+	firstName: "",
+	lastName: "",
+	email: "",
+	company: "",
+	description: "",
+	budget: "",
+	projectTime: "",
+}
+
+type ContactDataState = {
+	need: string
+	firstName: string
+	lastName: string
+	email: string
+	company: string
+	description: string
+	budget: string
+	projectTime: string
+}
+
+type ContactDataAction = {
+	type:
+		| "need"
+		| "firstName"
+		| "lastName"
+		| "email"
+		| "company"
+		| "description"
+		| "budget"
+		| "projectTime"
+	value?: string
+}
+
+function reducerContactData(
+	state: ContactDataState,
+	action: ContactDataAction
+) {
+	return {
+		...state,
+		[action.type]: action.value,
+	}
 }
 
 const imageLayout: ImageLayout = {
@@ -18,22 +64,26 @@ const imageLayout: ImageLayout = {
 
 export default function ContactLayout({
 	children,
-	params,
 }: {
 	children: React.ReactNode
-	params: {
-		tag: string
-		item: string
-	}
 }) {
 	const pathname =
 		usePathname().replaceAll("/", "").replace("contact", "") || "need"
 	const imageSrc = imageLayout[pathname]
 
+	const [contactData, dispatchContactData] = useReducer(
+		reducerContactData,
+		contactDataInit
+	)
+
 	return (
 		<section className="mx-auto mb-[5rem] flex max-w-[77.5rem] gap-[2.5rem] px-[1.5rem]">
 			<section className="mx-auto mt-[3.75rem] flex-1 md:mt-[5rem]">
-				{children}
+				<ContactDataContext.Provider
+					value={{ getter: contactData, setter: dispatchContactData }}
+				>
+					{children}
+				</ContactDataContext.Provider>
 				<ContactAction pathname={pathname} />
 			</section>
 			<figure className="relative hidden flex-1 md:block">
