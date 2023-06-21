87c978c0c8ab72309ddcda5a9d86293e19f3839b
reshaditiya <reshaditiya@gmail.com>
2023-06-21 14:47:32 +0700

    add contact action

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
-import Navbar from "./Navbar";
-import Footer from "./Footer";
+import Navbar from "./Navbar"
+import Footer from "./Footer"
+import ContactAction from "./ContactAction"
 
-export { Navbar, Footer };
+export { Navbar, Footer, ContactAction }
