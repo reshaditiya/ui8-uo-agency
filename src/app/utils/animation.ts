d7f109b984b6065faf5ac3784583646f44b838dc
reshaditiya <reshaditiya@gmail.com>
2023-06-24 07:42:37 +0700

    animation small

--- src/app/utils/animation.ts
+++ src/app/utils/animation.ts
@@ -4,6 +4,12 @@ export const defaultAnimation = {
 	exit: { y: 24, opacity: 0 },
 }
 
+export const defaultAnimationSmall = {
+	initial: { y: 8, opacity: 0 },
+	animate: { y: 0, opacity: 1 },
+	exit: { y: 8, opacity: 0 },
+}
+
 export const fadeAnimation = {
 	initial: { opacity: 0 },
 	animate: { opacity: 1 },
